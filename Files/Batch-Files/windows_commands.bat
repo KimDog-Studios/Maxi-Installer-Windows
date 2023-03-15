7a003068434cac3ec3e722fa3a7f40f6067cd3c1
KimDog-Studios <kimialbans54@gmail.com>
2023-03-15 00:48:05 +0000

    Update

--- Files/Batch-Files/windows_commands.bat
+++ Files/Batch-Files/windows_commands.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJGyX8VAjFDBdQSCNO1iuE7EV5/vHzOWDp3E2Xe8bd4jIl5mPKe5ey1X0ZtsE2GlOmccACQlkVRumSgAzuiNvt22LeuaMoxysY0aa8ko8F2Bmu1XVnjgyb917ppFTgXP+3kjrnuUm1GrsV6YAWFP0w6BhO+sV9AawVBqG2oRcRPLhfK6kIDnFLmgRiGOLpJN+l4wjXiw8AB1Cjtk=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 Title = "Windows Commands"
 CLS
