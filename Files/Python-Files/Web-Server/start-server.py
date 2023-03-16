af28879788ba120ca62bcd70631688c75e29d354
KimDog-Studios <kimialbans54@gmail.com>
2023-03-16 22:26:07 +0000

    Web-Progress

--- Files/Python-Files/Web-Server/start-server.py
+++ Files/Python-Files/Web-Server/start-server.py
@@ -1,19 +1,28 @@
 import http.server
 import socketserver
+import time
 
 PORT = 8000
+MESSAGE = "Hello from my Python web server!"
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def list_directory(self, path):
+        # Override to suppress directory listing
+        return []
+
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-
-        # Write the response content
-        response = "Hello, world!"
-        response = "Hello, world!"
-        self.wfile.write(response.encode())
+        self.wfile.write(bytes(f"<html><body><h1>{MESSAGE}</h1></body></html>", "utf-8"))
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-    print(f"serving at port {PORT}")
-    httpd.serve_forever()
+    print(f"Serving at port {PORT}")
+    while True:
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+            time.sleep(1)
